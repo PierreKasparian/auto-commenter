f30d3f0a647c948f7066069b7ceae3155548398c
kaspa <kasparianpierre@gmail.com>
2025-05-19 15:20:33 +0200

    rgereg

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,8 +1,14 @@
+export const dynamic = "force-dynamic";
 import { NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!
+);
 
 export async function GET(req: Request) {
-  console.log("coucou");
-  console.log(req.headers.get("Authorization"));
+  // console.log(process.env.N8N_AUTH)
   if (
     req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
   ) {
@@ -21,14 +27,54 @@ export async function GET(req: Request) {
     redirect: "follow",
   };
 
-  fetch("https://api12.unipile.com:14269/api/v1/accounts", requestOptions as RequestInit)
-    .then((response) => 
-        {console.log(response)
-            return response.text()})
-    .then((result) => console.log(result))
+  const accounts = await fetch(
+    "https://api12.unipile.com:14269/api/v1/accounts",
+    requestOptions as RequestInit
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      return result.items;
+    })
     .catch((error) => {
-        console.log(error)
-        return console.error(error)});
-  console.log("fin");
+      console.log(error);
+      return console.error(error);
+    });
+
+  console.log(accounts);
+  for (let account of accounts) {
+    const randomHour = Math.floor(Math.random() * 11) + 8; // 8 to 18
+    const randomMinute = Math.floor(Math.random() * 12) * 5; // 0, 5, 10, 15, ..., 55
+    const today = new Date();
+    today.setHours(randomHour, randomMinute, 0, 0);
+    console.log(today);
+    // const res = await fetch(
+    //   "http://localhost:5678/webhook-test/9f69c248-04f1-4057-90de-d8cd6d0b0c94",
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       "x-api-key": `${process.env.N8N_AUTH}`,
+    //     },
+    //     body: JSON.stringify({
+    //       account_id: account.id,
+    //       date: today,
+    //     }),
+    //   }
+    // );
+    const { data, error } = await supabase.from("comment-time").insert([
+      {
+        id: account.id,
+        comment_time: today.toISOString(),
+      },
+    ]);
+
+    if (error) {
+      console.error("Error inserting into Supabase:", error);
+    } else {
+      console.log("Successfully inserted task:", data);
+    }
+  }
   return NextResponse.json({ ok: true });
 }
