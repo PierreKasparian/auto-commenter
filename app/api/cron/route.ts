0834194532e3d4ed8731315de30459cf0ef610d4
kaspa <kasparianpierre@gmail.com>
2025-05-19 10:43:45 +0200

    sffs

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
+    console.log('coucou')
     console.log(req.headers.get('Authorization'))
     if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
         return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
