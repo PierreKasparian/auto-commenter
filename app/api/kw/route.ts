39166f8816e34ea5956020b07c45915392eb73ca
kaspa <kasparianpierre@gmail.com>
2025-05-22 15:43:01 +0200

    dz

--- app/api/kw/route.ts
+++ app/api/kw/route.ts
@@ -1,11 +1,11 @@
 export const dynamic = "force-dynamic";
 import { NextResponse } from "next/server";
-// import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-// const supabase = createClient(
-//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-// );
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 
 export async function POST(req: Request) {
   const body = await req.json();
@@ -15,8 +15,8 @@ export async function POST(req: Request) {
   ) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
-  // const account_id = body.account_id;
-  console.log("passé")
+  const account_id = body.account_id;
+  console.log("passé");
 
   const { data: keywords, error: keywordsError } = {
     data: { keywords: ["IA"] },
@@ -57,24 +57,29 @@ export async function POST(req: Request) {
   };
 
   const posts = await fetch(
-    "https://api12.unipile.com:14269/api/v1/linkedin/search?account_id=2lmjFJd6RjmKn4oA2VNbNA",
+    `https://api12.unipile.com:14269/api/v1/linkedin/search?account_id=${account_id}`,
     requestOptions as RequestInit
   )
     .then((response) => response.json())
     .catch((error) => console.error(error));
-  console.log(posts)
+  console.log(posts);
   for (const post of posts.items) {
-    console.log(post)
-    if (Number(post.date.slice(0, -1)) < 12) {//faiblesse dans l'approche
-      console.log("carré");
+    console.log(post);
+    if (Number(post.date.slice(0, -1)) <= 12 && post.text.split(' ').includes("IA")) {//faiblesse dans l'approche
+      const {error}= await supabase
+        .from("posts_comment")
+        .insert({
+          unipile_id: account_id,
+          post_text: post.text,
+          post_link:"youtube.com",
+          comment_IA:"test"
+        })
+        if(error)console.log(error);
     }
   }
 
-
   //2. parcourir le script voir si on trouve le mot clé
   //3. il faut commenter
-
-
 
   return NextResponse.json({ ok: true });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -29,5 +29,8 @@ export default async function Home() {
         }),
       });
     }
-  return
-}
+  return(
+  <>
+<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7329147539604045824" height="1388" width="504" frameBorder={0} allowFullScreen title="Post intégré"></iframe>
+  </>
+)}
