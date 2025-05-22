18b4e04aea37afb93b7d3bbc4884ba9dcdb9da1e
kaspa <kasparianpierre@gmail.com>
2025-05-22 18:19:38 +0200

    cleanassssss

--- app/api/kw/route.ts
+++ app/api/kw/route.ts
@@ -1,29 +1,44 @@
 export const dynamic = "force-dynamic";
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
-import { Groq } from 'groq-sdk';
+import { Groq } from "groq-sdk";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 const groq = new Groq();
-async function getComment(post:string) {
+async function generateComment(post: string) {
   const chatCompletion = await groq.chat.completions.create({
-    "messages": [
+    messages: [
       {
-        "role": "user",
-        "content": "Comment this post:\n"+post
-      }
+        role: "user",
+        content: "Comment this post:\n" + post,
+      },
     ],
-    "model": "meta-llama/llama-4-scout-17b-16e-instruct",
-    "temperature": 1,
-    "top_p": 1,
-    "stream": false,
-    "stop": null
+    model: "meta-llama/llama-4-scout-17b-16e-instruct",
+    temperature: 1,
+    top_p: 1,
+    stream: false,
+    stop: null,
   });
 
   return chatCompletion.choices[0]?.message.content;
+}
+
+async function createComment(
+  post: string,
+  post_link: string,
+  account_id: string
+) {
+  const comment = await generateComment(post);
+  const { error } = await supabase.from("posts_comment").insert({
+    unipile_id: account_id,
+    post_text: post,
+    post_link: post_link,
+    comment_IA: comment,
+  });
+  if (error) console.log(error);
 }
 export async function POST(req: Request) {
   const body = await req.json();
@@ -36,14 +51,11 @@ export async function POST(req: Request) {
   const account_id = body.account_id;
   console.log("passé");
 
-  const { data: keywords, error: keywordsError } = {
-    data: { keywords: ["IA"] },
-    error: null,
-  }; //await supabase
-  // .from("keywords")
-  // .select("keywords")
-  // .eq("unipile_id", account_id)
-  // .single();
+  const { data: keywords, error: keywordsError } = await supabase
+    .from("keywords")
+    .select("keywords")
+    .eq("unipile_id", account_id)
+    .single();
 
   if (keywordsError) {
     console.log(keywordsError);
@@ -58,7 +70,7 @@ export async function POST(req: Request) {
   myHeaders.append("accept", "application/json");
   myHeaders.append("content-type", "application/json");
 
-  const linkedInUrl = `https://www.linkedin.com/search/results/content/?datePosted="past-24h"&keywords=IA&origin=FACETED_SEARCH&sid=(p5&sortBy="relevance"`;
+  const linkedInUrl = `https://www.linkedin.com/search/results/content/?datePosted="past-24h"&keywords=${keywords.keywords.join("%20")}&origin=FACETED_SEARCH&sid=(p5&sortBy="relevance"`;
 
   const raw = JSON.stringify({
     api: "classic",
@@ -79,27 +91,14 @@ export async function POST(req: Request) {
   )
     .then((response) => response.json())
     .catch((error) => console.error(error));
-  console.log(posts);
   for (const post of posts.items) {
-    console.log(post);
-    if (Number(post.date.slice(0, -1)) <= 12) {//faiblesse dans l'approche
-      console.log("dedans")
-
-
-      
-      const comment = await getComment(post.text);
-      const {error}= await supabase
-        .from("posts_comment")
-        .insert({
-          unipile_id: account_id,
-          post_text: post.text,
-          post_link:"youtube.com",
-          comment_IA:comment
-        })
-        if(error)console.log(error);
+    if (Number(post.date.slice(0, -1)) <= 12) {
+      console.log(post);
+      //faiblesse dans l'approche
+      console.log("dedans");
+      createComment(post.text, post.share_url, account_id);
     }
   }
-
 
   return NextResponse.json({ ok: true });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,16 @@ import { Navbar } from "@/components/landing/navbar";
 import { PricingSection } from "@/components/landing/pricing-section";
 
 export default async function Home() {
-  // if(process.env.NEXT_ENV === "development"){
-  //   await fetch("https://auto-commenter.vercel.app/api/kw", {
-  //     method: "POST",
-  //     headers: {
-  //       Authorization: `Bearer ${process.env.TRIG_TASK_KEY}`,
-  //     },
-  //     body: JSON.stringify({
-  //       account_id: "HYRdNXu7QpK1eBVPxT2Qlg"
-  //     }),
-  //   });}
+  if(process.env.NEXT_ENV === "development"){
+    await fetch("https://auto-commenter.vercel.app/api/kw", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.TRIG_TASK_KEY}`,
+      },
+      body: JSON.stringify({
+        account_id: "HYRdNXu7QpK1eBVPxT2Qlg"
+      }),
+    });}
     
   return(
     <div className="min-h-screen bg-gradient-to-b from-white to-gray-50">
