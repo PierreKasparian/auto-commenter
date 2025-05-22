7d71e76035188f6dd2b117fb6fc61c3868342cc9
kaspa <kasparianpierre@gmail.com>
2025-05-22 11:10:01 +0200

    ef

--- app/api/kw/route.ts
+++ app/api/kw/route.ts
@@ -1,6 +1,6 @@
 export const dynamic = "force-dynamic";
 import { NextResponse } from "next/server";
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 
 // const supabase = createClient(
 //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
