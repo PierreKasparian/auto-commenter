8fba640f968da7d1290ec6d21ca67d49df7ee86c
kaspa <kasparianpierre@gmail.com>
2025-06-09 16:53:21 +0200

    test2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-export const maxDuration = 180;
+export const maxDuration = 60;
 export const dynamic = 'force-dynamic';
 import React from "react";
 import { Separator } from "@/components/ui/separator";
