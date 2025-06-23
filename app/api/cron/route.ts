c9317524b278d26c58ea7596be7f2dd890f4b6d8
kaspa <kasparianpierre@gmail.com>
2025-06-23 09:08:22 +0200

    hour set

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -17,7 +17,7 @@ function getRandomInt(min: number, max: number) {
 
 const generateRandomTime = (timezone: string) => {
   const time = new Date();
-  time.setHours(6);
+  time.setHours(4);
   const offsetMinutes = Math.max(getTimezoneOffsetInMinutes(timezone), -8 * 60);
 
   const timeofTimezone = time.getTime() + offsetMinutes * 60 * 1000;
