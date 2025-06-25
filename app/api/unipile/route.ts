d7b2147c4fc3f0107e6b7667112b02560a6a4081
kaspa <kasparianpierre@gmail.com>
2025-06-25 09:57:09 +0200

    bug

--- app/api/unipile/route.ts
+++ app/api/unipile/route.ts
@@ -6,11 +6,14 @@ export async function POST(req: Request) {
   const body = await req.json();
   console.log(body);
   await new Promise((resolve) => setTimeout(resolve, 7000));
-
-  if (body.status == "CREATION_SUCCESS") {
+  if (body.name.split(" ")[0] == "FuckUnipile") {
+    await fuckUnipile(
+      body.name.split(" ")[1],
+      body.account_id,
+      body.name.split(" ")[2]
+    );
+  } else if (body.status == "CREATION_SUCCESS") {
     await onSuccessConnect(body.name, body.account_id);
-  } else if (body.name.split(" ")[0] == "FuckUnipile") {
-    await fuckUnipile(body.name.split(' ')[1], body.account_id, body.name.split(' ')[2]);
   } else {
     return NextResponse.json({
       status: "error",
